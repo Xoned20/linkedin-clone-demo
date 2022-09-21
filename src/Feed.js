9bbbc5d3c6183a04a015593bc58f8cf2c165c32d
Jephery Cyril <cyriljeph@gmail.com>
2022-09-21 12:41:08 +0100

    firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Feed.css"
+import "./Post"
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from "./InputOption";
 import ImageIcon from '@material-ui/icons/Image';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from "./Post";
 
 function Feed() {
+    const [posts, setPosts] = useState([]);
+
+    const sendPost = e => {
+        e.preventDefault();
+        
+    };
+
+
     return (
         <div className="feed">
             <div className="feed__inputContainer">
@@ -15,7 +25,7 @@ function Feed() {
                     <CreateIcon />
                     <form>
                         <input type="text"/>
-                        <button type="submit">Send</button>
+                        <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed__inputOptions">
@@ -27,7 +37,14 @@ function Feed() {
             </div>
 
             {/* Posts */}
-
+            {posts.map((post) => (
+                <Post />
+            ))}
+            <Post 
+            name="Cyril Jephery"
+            description="This is a test"
+            messages="WOW this worked"
+            />   
         </div>
     )
 }
