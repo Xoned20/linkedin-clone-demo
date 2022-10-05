e0a4af37b623b3b8a5be2374a3994e030b0669e3
Jephery Cyril <cyriljeph@gmail.com>
2022-10-05 13:50:07 +0100

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCuZcDmL7SgQQhVnGrZt5vLL3m3k3cP9MM",
+    authDomain: "linkedin-clone-demo-32c1c.firebaseapp.com",
+    projectId: "linkedin-clone-demo-32c1c",
+    storageBucket: "linkedin-clone-demo-32c1c.appspot.com",
+    messagingSenderId: "540134573222",
+    appId: "1:540134573222:web:4c33e807a60e1ab49ea067"
+  };
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
